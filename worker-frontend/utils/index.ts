3bfd8e53bd7146cb3717670581072e0dc53dd897
raghav Dutta <raghav.l.ldutt@gmail.com>
2024-06-26 13:00:46 +0530

    build edits in worker-frontend

--- worker-frontend/utils/index.ts
+++ worker-frontend/utils/index.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://quezz.vercel.app";
+export const BACKEND_URL = "https://quezz-backend.onrender.com";
 export const CLOUDFRONT_URL = "https://d272xdou7j7ajc.cloudfront.net"
