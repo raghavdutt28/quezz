7f8c167803e7bd426ac4bbcfafb2abae92b5c72d
raghav Dutta <raghav.l.ldutt@gmail.com>
2024-10-07 20:13:46 +0530

    utlity fixes

--- worker-frontend/utils/index.ts
+++ worker-frontend/utils/index.ts
@@ -1,3 +1,3 @@
-//export const BACKEND_URL = "https://quezz-backend.onrender.com";
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "https://quezz-backend.onrender.com";
+//export const BACKEND_URL = "http://localhost:8080";
 export const CLOUDFRONT_URL = "https://d272xdou7j7ajc.cloudfront.net"
